44c32c62613f311ccdc150db896c6fd835b0b709
LaibaArif <laibaslatch@gmail.com>
2023-04-25 14:48:15 +0500

    LinkPagesAndAddingwebPages

--- Site1.Master.cs
+++ Site1.Master.cs
@@ -13,5 +13,36 @@ namespace FinalProject
         {
 
         }
+
+        protected void LinkButton6_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminlogin.aspx");
+        }
+
+        protected void LinkButton11_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ViewSpareParts.aspx");
+        }
+
+        protected void LinkButton12_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ViewManufacturer.aspx");
+        }
+
+        protected void LinkButton4_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ViewSpareParts.aspx");
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+
+            Response.Redirect("SignUp.aspx");
+        }
     }
 }
--- Site1.Master.designer.cs
+++ Site1.Master.designer.cs
@@ -42,15 +42,6 @@ namespace FinalProject
         protected global::System.Web.UI.WebControls.LinkButton LinkButton4;
 
         /// <summary>
-        /// LinkButton1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
-
-        /// <summary>
         /// LinkButton2 control.
         /// </summary>
         /// <remarks>
@@ -112,14 +103,5 @@ namespace FinalProject
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton LinkButton12;
-
-        /// <summary>
-        /// LinkButton8 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton8;
     }
 }
